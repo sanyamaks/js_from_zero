4569b96038cb06797dfdaf5486cf336272dd72f2
Aleksandr <sanyamaks14@gmail.com>
2019-12-18 21:21:35 +0400

    Seventh task (Countdown timer)

--- 7_task/js/script.js
+++ 7_task/js/script.js
@@ -2,6 +2,8 @@ let infoHeaderTab = document.getElementsByClassName("info-header-tab");
 let infoHeader = document.getElementsByClassName("info-header")[0];
 let infoTabcontent = document.getElementsByClassName("info-tabcontent");
 
+hideTabContent(1);
+
 function hideTabContent(a) {
   for (let id = a; id < infoTabcontent.length; id++) {
     infoTabcontent[id].classList.remove("show");
@@ -27,3 +29,53 @@ infoHeader.addEventListener("click", function() {
     }
   }
 });
+
+let deadline = new Date(1576689800000); //hardcode
+getTimeRemaining(deadline);
+
+function changeTimeFormat(time) {
+  time = time < 10 ? "0" + time : "" + time;
+  return time;
+}
+
+function getTimeRemaining(endtime) {
+  let t = Date.parse(endtime) - Date.parse(new Date());
+  let seconds =
+    t > 0 ? changeTimeFormat(Math.floor((t / 1000) % 60)) : changeTimeFormat(0);
+  let minutes =
+    t > 0
+      ? changeTimeFormat(Math.floor((t / 1000 / 60) % 60))
+      : changeTimeFormat(0);
+  let hours =
+    t > 0
+      ? changeTimeFormat(Math.floor(t / 1000 / 60 / 60))
+      : changeTimeFormat(0);
+  return {
+    total: t,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+  };
+}
+
+function setClock(id, endtime) {
+  let timer = document.getElementById(id);
+  let hours = timer.querySelector(".hours");
+  let minutes = timer.querySelector(".minutes");
+  let seconds = timer.querySelector(".seconds");
+
+  let timeInterval = setInterval(updateClock, 1000);
+
+  function updateClock() {
+    let timeRemaining = getTimeRemaining(endtime);
+    hours.textContent = timeRemaining.hours;
+    minutes.textContent = timeRemaining.minutes;
+    seconds.textContent = timeRemaining.seconds;
+
+    if (timeRemaining.total < 0) {
+      clearInterval(timeInterval);
+    }
+  }
+}
+
+setClock("timer", deadline);
